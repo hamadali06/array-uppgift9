72624b70ebc190df5de1771a200ba8604eadadb3
Emad Iddin Ali <ea224pn@student.lnu.se>
2024-06-05 19:16:41 +0200

    Add project files.

---
+++ array uppgift9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Inlamning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> namn = new List<string>();
+            while (true)
+            {
+                Console.WriteLine("Skriv in ett namn eller tryck på den knappen som kallas tror jag enter:");
+                string inNamn = Console.ReadLine();
+                if (string.IsNullOrEmpty(inNamn)) break;
+                namn.Add(inNamn);
+            }
+            Console.WriteLine("Skriv ett namn att söka efter:");
+            string sökNamn = Console.ReadLine();
+            int antal = namn.Count(n => n == sökNamn);
+            Console.WriteLine($"Namnet {sökNamn} förekommer så här många gånger {antal} gånger.");
+        }
+    }
+}
